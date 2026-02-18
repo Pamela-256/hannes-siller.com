f4cbcd14a161c8a6f19538efc9f8af154c7cd5a2
polyerror <hannes@hannes-siller.com>
2026-02-18 20:04:06 +0100

    implemented a json file with further description infos and the possibility to change the order of images, if the "info.json" file does not exist images wil be sorted alphabetically

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import json
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -16,7 +18,19 @@ def index():
 def project(name):
     # Find all images in the specific folder
     path = os.path.join(PROJECTS_DIR, name)
-    images = [f for f in os.listdir(path) if f.lower().endswith(('.jpg', '.jpeg', '.png'))]
+    json_path = os.path.join(path, 'info.json')
+
+    if os.path.exists(json_path):
+        with open(json_path, 'r') as f:
+            data = json.load(f)
+            images = data['order'] # Uses your custom order
+            title = data.get('title', name)
+    else:
+        images = [f for f in os.listdir(path) if f.lower().endswith(('.jpg', '.jpeg', '.png'))]
+
+        # sort images from 0-9 and then A-Z
+        images.sort() 
+        
     return render_template('project.html', name=name, images=images)
 
 if __name__ == '__main__':
